de2106df02d5414db58955a61e169b79fcc03df9
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-04 17:44:50 +0100

    Update

--- MS4_test/settings.py
+++ MS4_test/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-todo-app-telesia.herokuapp.com/']
+ALLOWED_HOSTS = ['django-todo-app-telesia.herokuapp.com']
 
 
 # Application definition
